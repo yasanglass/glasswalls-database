313a41477a203afe7a4ed72464bd1818b2915ef2
Yasan Glass <contact@yasan.glass>
2023-08-05 12:59:16 +0200

    Update schema

--- glasswalls.sql
+++ glasswalls.sql
@@ -1,4 +1,16 @@
 BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "artist" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "collection" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "inspiration" (
+	"is"	INTEGER NOT NULL UNIQUE,
+	PRIMARY KEY("is" AUTOINCREMENT)
+);
 CREATE TABLE IF NOT EXISTS "wallpaper" (
 	"id"	INTEGER NOT NULL UNIQUE,
 	"title"	TEXT UNIQUE,
@@ -6,6 +18,12 @@ CREATE TABLE IF NOT EXISTS "wallpaper" (
 	"height"	INTEGER NOT NULL,
 	"year"	INTEGER,
 	"month"	INTEGER,
-	PRIMARY KEY("id" AUTOINCREMENT)
+	"collection_id"	INTEGER,
+	"inspiration_id"	INTEGER,
+	"artist_id"	INTEGER,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("collection_id") REFERENCES "collection"("id"),
+	FOREIGN KEY("inspiration_id") REFERENCES "inspiration"("is"),
+	FOREIGN KEY("artist_id") REFERENCES "artist"("id")
 );
 COMMIT;
